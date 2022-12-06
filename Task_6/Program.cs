1dfa88957ae59b5932e723ebd7412e64d6129488
Vitaly <wiatly@mail.ru>
2022-12-06 18:36:40 +0300

    Task_6 done

---
+++ Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string? numLine_1 = Console.ReadLine();
+
+if (numLine_1 != null)
+{
+    int inputNum_1 = int.Parse(numLine_1);
+
+
+    if (inputNum_1 % 2 == 0)
+    {
+        Console.WriteLine($"Число \t{inputNum_1} ,четное");
+    }
+
+    else
+    {
+        Console.WriteLine($"Число \t{inputNum_1} ,нечетное");
+    }
+
+}
