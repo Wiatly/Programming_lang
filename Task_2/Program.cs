5efdfc565f5944217de205bc74f6c20533795e33
Vitaly <wiatly@mail.ru>
2022-12-06 17:54:25 +0300

    Task_2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿string? numLine_1 = Console.ReadLine();
+string? numLine_2 = Console.ReadLine();
+
+if (numLine_1 != null && numLine_2 != null)
+{
+    int inputNum_1 = int.Parse(numLine_1);
+    int inputNum_2 = int.Parse(numLine_2);
+
+
+    if (inputNum_1 > inputNum_2)
+    {
+        Console.WriteLine($"Первое число \t{inputNum_1} ,больше второго {inputNum_2}");
+    }
+    else
+    {
+        Console.WriteLine($"Второе число \t{inputNum_2} ,больше пераого {inputNum_1}");
+    }
+}
