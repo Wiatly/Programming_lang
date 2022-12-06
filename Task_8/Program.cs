65776860bc8e351f44a06e44f417188297a321af
Vitaly <wiatly@mail.ru>
2022-12-06 18:59:59 +0300

    Task_8 done

---
+++ Task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+string? numLine_1 = Console.ReadLine();
+int evenNum = 2;
+
+if (numLine_1 != null)
+{
+    int inputNum_1 = int.Parse(numLine_1);
+    if (inputNum_1 >= 2)
+    {
+        do
+        {
+            Console.Write(evenNum);
+            Console.Write(", ");
+            evenNum = evenNum + 2;
+        }
+        while (evenNum < inputNum_1);
+        if (inputNum_1 % 2 == 0) Console.Write(evenNum);
+    }
+    else Console.WriteLine("Введеное число меньше 2");
+
+
+}
