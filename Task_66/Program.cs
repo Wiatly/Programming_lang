c7358a71d58cf2c17850947995d97038628c55e7
Vitaly <wiatly@mail.ru>
2023-01-09 17:29:25 +0300

    Task_64, 66, 68 done

---
+++ Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+
+int RecMN(int M, int N)
+{
+    int outRes = 0;
+    if (M >= N) return outRes + N;
+    outRes = M + RecMN(M + 1, N);
+    return outRes;
+}
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+int resultLine = number1 < number2 ? RecMN(number1, number2) : RecMN(number2, number1);
+Console.WriteLine($"Сумма натуральных чисел между {number1} и {number2}: {resultLine}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+
+long Ackerman(long m, long n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ackerman(m - 1, 1);
+  else return Ackerman(m - 1, Ackerman(m, n - 1));
+}
+
+long number1 = ReadData("Введите число m: ");
+long number2 = ReadData("Введите число n: ");
+long result=Ackerman(number1,number2);
+Console.WriteLine($"Значение функции Аккермана: {result}");
